55b0e0b34789fc6452bb9d08d2b1b316b8cbe496
Артем Римша <artem-rimsha@mail.ru>
2022-07-14 17:44:42 +0300

    Новый коммит

--- yatube/posts/forms.py
+++ yatube/posts/forms.py
@@ -6,10 +6,6 @@ from .models import Post
 class PostForm(ModelForm):
     class Meta:
         model = Post
-        labels = {'group': 'Группа', 
-                  'text': 'Сообщение',
-        }
-        help_texts = {'group': 'Выберите группу', 
-                      'text': 'Введите ссообщение',
-        }
+        labels = {'group': 'Группа', 'text': 'Сообщение'}
+        help_texts = {'group': 'Выберите группу', 'text': 'Введите ссообщение'}
         fields = ["group", "text"]
