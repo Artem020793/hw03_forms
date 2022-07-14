2ad3d362def940fc41aceff293b9d0558bc0c3ab
Артем Римша <artem-rimsha@mail.ru>
2022-07-14 17:47:52 +0300

    Новый коммит

--- yatube/users/forms.py
+++ yatube/users/forms.py
@@ -8,8 +8,8 @@ User = get_user_model()
 class CreationForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
         model = User
-        fields = ('first_name','last_name','username','email')
+        fields = ('first_name', 'last_name', 'username', 'email')
 
 
 class PasswordChangingForm(PasswordChangeForm):
-    fields = ('now_password','new_password','new_password_again')
+    fields = ('now_password', 'new_password', 'new_password_again')
