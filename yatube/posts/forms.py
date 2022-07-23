a533b35f66e88157b3426d438c9b57a53a5c7d7a
Артем Римша <artem-rimsha@mail.ru>
2022-07-23 12:07:18 +0300

    Все исправил

--- yatube/posts/forms.py
+++ yatube/posts/forms.py
@@ -7,5 +7,6 @@ class PostForm(ModelForm):
     class Meta:
         model = Post
         labels = {'group': 'Группа', 'text': 'Текст поста'}
-        help_texts = {'group': 'Выберите группу', 'text': 'Введите текст поста'}
+        help_texts = {'group': 'Выберите группу', 
+                      'text': 'Введите текст поста'}
         fields = ['text', 'group']
